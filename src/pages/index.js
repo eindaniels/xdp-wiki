ad28ad813542377550f7d19a19704cec10df5a2c
DaLoL <dalol.dev@gmail.com>
2023-06-05 17:21:45 +0200

    ddd

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="CA Wiki 2.0 - jetzt in der Alpha version.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
