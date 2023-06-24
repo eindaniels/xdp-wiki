c6be29ccf32259eca9528de2d268278f830e45da
xdalul <dalol.dev@gmail.com>
2023-06-24 18:52:55 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,32 +69,32 @@ const config = {
           alt: 'CA Logo',
           src: 'img/logo.png',
         },
-        algolia: {
-          // The application ID provided by Algolia
-          appId: 'K6KQWQITJ3',
-    
-          // Public API key: it is safe to commit it
-          apiKey: '13e374665649da08fb519392ecac2b61',
-    
-          indexName: 'craftattack',
-    
-          // Optional: see doc section below
-          contextualSearch: true,
-    
-          // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-          replaceSearchResultPathname: {
-            from: '/docs/', // or as RegExp: /\/docs\//
-            to: '/',
-          },
-    
-          // Optional: Algolia search parameters
-          searchParameters: {},
-    
-          // Optional: path for search page that enabled by default (`false` to disable it)
-          searchPagePath: 'search',
-    
-          //... other Algolia params
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'K6KQWQITJ3',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '13e374665649da08fb519392ecac2b61',
+  
+        indexName: 'craftattack',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
         },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
         items: [
           {
             type: 'docSidebar',
