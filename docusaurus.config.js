8c875cd6042b5a7db8f75ced3b1fa769ef837513
xdalul <dalol.dev@gmail.com>
2023-07-11 15:13:48 +0200

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -163,9 +163,9 @@ const config = {
       announcementBar: {
         id: 'achtung',
         content:
-          'Achtung! Dieses Wiki ist noch in der Alpha, bei Fehlern usw. bitte im <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/TMNUqyKz5T">Discord</a> melden.',
-        backgroundColor: '#ffffff',
-        textColor: '#deeaff',
+          '🚨 Achtung! Dieses Wiki ist noch in der Alpha, bei Fehlern usw. bitte im <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/TMNUqyKz5T">Discord</a> melden. 🚨',
+        backgroundColor: '#eb4242',
+        textColor: '#ffffff',
         isCloseable: true,
       },  
     }),
