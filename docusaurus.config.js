3bfc17d39ec12d03cebb0f761f816005c9e72d1a
dalol <101058306+xdalul@users.noreply.github.com>
2024-04-23 21:13:35 +0200

    version testing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -192,19 +192,6 @@ const config = {
 
 module.exports = config;
 
+import { inject } from '@vercel/analytics';
 
-import { Analytics } from '@vercel/analytics/react';
-
-export default function RootLayout({ children }) {
-  return (
-      <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-      {children}
-      <Analytics />
-      </body>
-      </html>
-  );
-}
+inject();
