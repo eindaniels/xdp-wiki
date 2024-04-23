cb379580da63db2860291bb9556c118d556cd69d
dalol <101058306+xdalul@users.noreply.github.com>
2024-04-23 20:30:02 +0200

    version testing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,10 +52,6 @@ const config = {
               label: '1.0.0',
               path: '1.0.0',
             },
-            "1.1.0": {
-              label: '1.1.0',
-              path: '1.1.0',
-            }
           },
         },
         blog: {
