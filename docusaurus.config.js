d7dcc47d4731d7a6647fb3f43842d3ba538e6ce0
xdalul <101058306+xdalul@users.noreply.github.com>
2024-05-18 01:07:40 +0200

    Ready to Release Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,16 +59,11 @@ const config = {
           showLastUpdateTime: true,
           lastVersion: 'current',
           versions: {
-            "prev1": {
-              label: 'Pre 1.0.0',
-              path: 'prev1',
+            "1.0.0": {
+              label: '1.0.0',
+              path: '1.0.0',
               banner: 'unmaintained',
             },
-            "1.1.0": {
-              label: '1.1.0',
-              path: '1.1.0',
-              banner: 'unreleased',
-            }
           },
         },
         blog: {
